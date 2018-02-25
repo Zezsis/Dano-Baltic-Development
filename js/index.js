f47167c9b4e01f6be353d69753f0aa9143300c58
Zezsis <36262453+Zezsis@users.noreply.github.com>
2018-02-25 09:45:15 +0100

    removed ascii

--- js/index.js
+++ js/index.js
@@ -1,28 +1,6 @@
 // Based on an example:
 //https://github.com/don/cordova-plugin-ble-central
 
-
-// ASCII only
-function bytesToString(buffer) {
-    return String.fromCharCode.apply(null, new Uint8Array(buffer));
-}
-
-// ASCII only
-function stringToBytes(string) {
-    var array = new Uint8Array(string.length);
-    for (var i = 0, l = string.length; i < l; i++) {
-        array[i] = string.charCodeAt(i);
-    }
-    return array.buffer;
-}
-
-// this is ble hm-10 UART service
-/*var blue= {
-    serviceUUID: "0000FFE0-0000-1000-8000-00805F9B34FB",
-    characteristicUUID: "0000FFE1-0000-1000-8000-00805F9B34FB"
-};*/
-
-//the bluefruit UART Service
 var blue = {
     serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
     txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
